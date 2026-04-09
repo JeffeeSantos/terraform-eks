90f2799444d8d1562053d75790f297d1da0ba682
JeffeeSantos <usergamehs@gmail.com>
2026-04-09 07:13:23 -0300

    feat: adding output for new module MNG

--- modules.tf
+++ modules.tf
@@ -12,3 +12,12 @@ module "eks_cluster" {
   public_subnet_1a = module.eks_network.public_subnet_1a
   public_subnet_1b = module.eks_network.public_subnet_1b
 }
+
+module "eks_managed_node_group" {
+  source            = "./modules/managed-node-group"
+  project_name      = var.project_name
+  cluster_name      = module.eks_cluster.cluster_name
+  private_subnet_1a = module.eks_network.subnet_priv_1a
+  private_subnet_1b = module.eks_network.subnet_priv_1b
+  tags              = local.tags
+}
